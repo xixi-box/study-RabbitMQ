80872f3718adeeedb2a93530d0e38865e2d1987c
ws <2606209307@qq.com>
2023-05-25 21:02:48 +0800

    Initial commit

---
+++ src/main/java/com/ws/Main.java
@@ -0,0 +1,12 @@
+package com.ws;
+
+/**
+ * @Author: 王顺
+ * @Date: 2023/5/25 21:02
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
